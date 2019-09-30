bd85570d16eac7760391f8d3617c708c8c701404
zhoubing <1050015785@qq.com>
2019-09-30 10:37:34 +0800

    周兵 清空项目 重新开始

--- src/main/java/com/cskaoyan/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.cskaoyan.mapper;
-
-import org.apache.ibatis.annotations.Select;
-
-public interface UserMapper {
-    @Select("select username from user where id = #{id}")
-    String queryUserNameById(int id);
-}
