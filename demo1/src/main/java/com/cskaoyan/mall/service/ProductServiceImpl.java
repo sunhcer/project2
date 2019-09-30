7f4e921729bfc6becbd2cc18da388bdee95010bf
egge10032 <741185698@qq.com>
2019-09-30 17:34:08 +0800

    陈宇齐 新增显示商品接口和商品分页类

--- demo1/src/main/java/com/cskaoyan/mall/service/ProductServiceImpl.java
+++ demo1/src/main/java/com/cskaoyan/mall/service/ProductServiceImpl.java
@@ -1,10 +1,12 @@
 package com.cskaoyan.mall.service;
 
-import com.cskaoyan.mall.bean.Brand;
-import com.cskaoyan.mall.bean.Category;
+import com.cskaoyan.mall.bean.*;
 import com.cskaoyan.mall.mapper.BrandMapper;
 import com.cskaoyan.mall.mapper.CategoryMapper;
+import com.cskaoyan.mall.mapper.GoodsMapper;
 import com.cskaoyan.mall.vo.CatAndBrandVo;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +26,10 @@ public class ProductServiceImpl implements  ProductService {
 
     @Autowired
     CategoryMapper categoryMapper;
+
+    @Autowired
+    GoodsMapper goodsMapper;
+
     @Override
     public List<CatAndBrandVo> findAllBrandToVo() {
         return brandMapper.findAllBrandToVo();
@@ -32,5 +38,27 @@ public class ProductServiceImpl implements  ProductService {
     @Override
     public List<CatAndBrandVo> findAllCategoriesToVo() {
         return categoryMapper.findAllCategoriesToVo();
+    }
+
+    @Override
+    public List<Goods> findAllGoods() {
+        return goodsMapper.findAllGoods();
+    }
+
+    @Override
+    public Integer findAmountOfGoods() {
+        return goodsMapper.findAmountOfGoods();
+    }
+
+    @Override
+    public GoodsList findGoodsByPage(GoodsPage page) {
+        PageHelper.startPage(page.getPage(),page.getLimit(),page.getDesc());
+        List<Goods> goods=goodsMapper.findAllGoods();
+        PageInfo<Goods> goodsPageInfo=new PageInfo<>(goods);
+        long total=goodsPageInfo.getTotal();
+        GoodsList goodsList=new GoodsList();
+        goodsList.setItems(goods);
+        goodsList.setTotal(total);
+        return goodsList;
     }
 }
