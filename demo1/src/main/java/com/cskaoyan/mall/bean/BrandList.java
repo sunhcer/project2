35d0c38e91818533e99c6d48b485ac0bf10bc311
zhoubing <1050015785@qq.com>
2019-09-30 15:58:38 +0800

    周兵 增加依赖中的文件上传组件

---
+++ demo1/src/main/java/com/cskaoyan/mall/bean/BrandList.java
@@ -0,0 +1,10 @@
+package com.cskaoyan.mall.bean;
+
+/**
+ * @Description:
+ * @Author: zhou
+ * @Date: 2019/9/30
+ * @Time 15:51
+ */
+public class BrandList {
+}
---
+++ demo1/src/main/java/com/cskaoyan/mall/service/BrandService.java
@@ -0,0 +1,10 @@
+package com.cskaoyan.mall.service;
+
+/**
+ * @Description:
+ * @Author: zhou
+ * @Date: 2019/9/30
+ * @Time 14:50
+ */
+public class BrandService {
+}
