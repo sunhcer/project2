32e1af68167d1b7252bf6f8dc037bada488310a7
egge10032 <741185698@qq.com>
2019-10-05 21:48:13 +0800

    陈宇齐 新增分类页面，修正首页逻辑错误

--- demo1/src/main/java/com/cskaoyan/mall/mapper/CategoryMapper.java
+++ demo1/src/main/java/com/cskaoyan/mall/mapper/CategoryMapper.java
@@ -25,4 +25,6 @@ public interface CategoryMapper {
     List<Category> findAllCateGoriesByLevel(String level);
 
     void deleteById(Integer id);
+
+    List<Category> findByParentId(Integer pid);
 }
