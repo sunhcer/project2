5a8065fff4e82be9c9a3f88e4045cc6bbd5c4ab3
ywx <18585618181@163.com>
2019-10-07 20:10:52 +0800

    ywx search helper

--- demo1/src/main/java/com/cskaoyan/mall/controller/wx/WxSearchController.java
+++ demo1/src/main/java/com/cskaoyan/mall/controller/wx/WxSearchController.java
@@ -1,10 +1,14 @@
 package com.cskaoyan.mall.controller.wx;
 
+import com.cskaoyan.mall.service.wx.WxSearchService;
 import com.cskaoyan.mall.vo.BaseRespVo;
 import com.fasterxml.jackson.databind.ser.Serializers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 类简介：
@@ -16,9 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WxSearchController {
 
+    @Autowired
+    WxSearchService wxSearchService;
+
     @RequestMapping("wx/search/index")
     public BaseRespVo searchIndex(){
     return null;
     }
 
+    @RequestMapping("/wx/search/helper")
+    public BaseRespVo searchHelper(String keyword) {
+        List<String> keywords = wxSearchService.searchHelper(keyword);
+        return BaseRespVo.success(keywords);
+    }
 }
